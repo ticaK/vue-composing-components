6f58e90b13755722d5588bac896c72f3910b34a9
Tijana Koprivica <tijanak1991@hotmail.com>
2019-03-18 21:40:36 +0100

    a

--- src/main.js
+++ src/main.js
@@ -3,6 +3,18 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
+// globalni mixin necemo koristiti
+
+Vue.mixin({
+  created(){
+    console.log(' created from global mixin');
+  },
+  mounted(){
+    console.log('mounted from global mixin');
+  }
+
+})
+
 new Vue({
   render: h => h(App),
 }).$mount('#app')
